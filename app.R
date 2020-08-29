b93dd4ab08ae2e187eac19091dc44f7d1bf71057
GuilhermeFCO <guifernandesco@hotmail.com>
2020-08-29 15:35:48 -0300

    juros simples

--- app.R
+++ app.R
@@ -2,7 +2,27 @@ library(shiny)
 library(shinythemes)
 
 ui <- navbarPage("IMFAI", theme = shinytheme("darkly"),
-  tabPanel("Juros Simples"),
+  tabPanel("Juros Simples",
+    sidebarLayout(
+      sidebarPanel(
+        numericInput("c",
+          "Valor Inicial",
+          0,00
+        ),
+        numericInput("i",
+          "Taxa de Juros (%)",
+          0,00
+        ),
+        numericInput("t",
+          "Tempo da Aplicação",
+          0,00
+        )
+      ),
+      mainPanel(
+               
+      )
+    )
+  ),
   tabPanel("Juros Compostos"),
   tabPanel("Sistema SAC"),
   tabPanel("Sistema Price")
