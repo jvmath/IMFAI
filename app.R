c845ae41e49c69eb80542fac0ee85d716a6d8a64
jvmath <jose.novaes@ufv.br>
2020-11-27 15:41:36 -0300

    g

--- app.R
+++ app.R
@@ -1,112 +1,219 @@
 library(shiny)
 library(shinythemes)
-
-ui <- navbarPage("IMFAI", theme = shinytheme("darkly"),
-  tabPanel("Juros Simples",
-    sidebarLayout(
-      sidebarPanel(
-        numericInput("cJS",
-          "Valor Inicial",
-          0,00
-        ),
-        numericInput("iJS",
-          "Taxa de Juros (%)",
-          0,00
-        ),
-        numericInput("tJS",
-          "Tempo da Aplicação",
-          0,00
-        )
-      ),
-      mainPanel(
-        h1(textOutput("resultadoJS"))
-      )
-    )
-  ),
-  tabPanel("Juros Compostos",
-           sidebarLayout(
-             sidebarPanel(
-               numericInput("cJC",
-                            "Valor Inicial",
-                            0,00
-               ),
-               numericInput("iJC",
-                            "Taxa de Juros (%)",
-                            0,00
-               ),
-               numericInput("tJC",
-                            "Tempo da Aplicação",
-                            0,00
-               )
-             ),
-             mainPanel(
-               h1(textOutput("resultadoJSC"))
-             )
-           )
-  ),
-  tabPanel("Sistema SAC",
-           sidebarLayout(
-             sidebarPanel(
-               numericInput("value",
-                            "Valor do Emprestimo",
-                            0,00
-               ),
-               numericInput("i",
-                            "Taxa de Juros (%)",
-                            0,00
-               ),
-               numericInput("k",
-                            "Tempo da Aplicação em meses",
-                            0,00
-               )
-             ),
-             mainPanel(
-               tableOutput("resultadoSA"),
-               h1(textOutput("msg"))
-             )
-           )
-  ),
-  tabPanel("Sistema Price")
+library(stringr)
+library(data.table)
+library(png)
+library(markdown)
+ui <- navbarPage("IMFAI", theme = shinytheme("sandstone"),
+                 tabPanel("Inicio" 
+                          ),
+                          ##
+                 tabPanel("Juros Simples",
+                          sidebarLayout(
+                            sidebarPanel(
+                              numericInput("cJS",
+                                           "Valor Inicial",
+                                           0,00
+                              ),
+                              numericInput("iJS",
+                                           "Taxa de Juros mensal (%)",
+                                           0,00
+                              ),
+                              numericInput("tJS",
+                                           "Tempo da Aplicação em meses",
+                                           0,00
+                              )
+                            ),
+                            mainPanel(
+                              h1(textOutput("resultadoJS"))
+                            )
+                          )
+                 ),
+                 tabPanel("Juros Compostos",
+                          sidebarLayout(
+                            sidebarPanel(
+                              numericInput("cJC",
+                                           "Valor Inicial",
+                                           0,00
+                              ),
+                              numericInput("iJC",
+                                           "Taxa de Juros mensal (%)",
+                                           0,00
+                              ),
+                              numericInput("tJC",
+                                           "Tempo da Aplicação em meses",
+                                           0,00
+                              )
+                            ),
+                            mainPanel(
+                              h1(textOutput("resultadoJC"))
+                            )
+                          )
+                 ),
+                 
+                 tabPanel("Sistema SAC",
+                          sidebarLayout(
+                            sidebarPanel(
+                              numericInput("value",
+                                           "Valor do Emprestimo",
+                                           0,00
+                              ),
+                              numericInput("i",
+                                           "Taxa de Juros (%)",
+                                           0,00
+                              ),
+                              numericInput("k",
+                                           "Tempo da Aplicação em meses",
+                                           0,00
+                              )
+                            ),
+                            mainPanel(
+                              DT::dataTableOutput("resultadoSA"),
+                              h1(textOutput("msg"))
+                            )
+                          )
+                 ),
+                 
+                 tabPanel("Sistema Price",
+                          sidebarLayout(
+                            sidebarPanel(
+                              numericInput("valuep",
+                                           "Valor do Emprestimo",
+                                           0
+                                           
+                              ),
+                              numericInput("ip",
+                                           "Taxa de Juros (%)",
+                                           0
+                              ),
+                              numericInput("kp",
+                                           "Tempo da Aplicação",
+                                           0
+                              ),
+                              textOutput("resultadoP")
+                            ),
+                            mainPanel(
+                              textOutput("NoValue"),
+                              DT::dataTableOutput("resultadoPR")
+                            )
+                          )
+                 ),
+                 tabPanel("Sobre", icon = icon("comment", lib = "glyphicon"),
+                          includeMarkdown("sobre.md")),
+                 tabPanel("Analise Financeira",
+                          p("Pagina criada para o projeto TCC...") )
 )
 
+
 server <- function(input, output) {
-  output$resultadoSA <- renderTable({
-    
-    D <- matrix(nrow = input$k, ncol = 1)
-    P <- matrix(nrow = input$k, ncol = 1)
-    J <- matrix(nrow = input$k, ncol = 1)
-    for(l in 1:((input$k)-1)){
-      A <- input$value/input$k
-      D0 <- input$value
-      D[1,] <- D0-A
-      D[l+1,] <- D[l,]-A
+  output$resultadoSA <- DT::renderDataTable({
+    if(input$value=="0"||input$i=="0"||input$k=="0"){
       
-      J[1,] <- D0*(input$i/100)
-      J[l+1,] <- D[l,]*(input$i/100)
+      as.data.table(paste("Preencha os valores da coluna lateral"))
       
-      P[l,] <- J[l,]+A
-      P[input$k,] <- J[input$k,]+A
+      
+    }else{
+      
+      D <- matrix(nrow = input$k, ncol = 1)
+      P <- matrix(nrow = input$k, ncol = 1)
+      J <- matrix(nrow = input$k, ncol = 1)
+      for(l in 1:((input$k)-1)){
+        A <- input$value/input$k
+        D0 <- input$value
+        D[1,] <- D0-A
+        D[l+1,] <- D[l,]-A
+        
+        J[1,] <- D0*(input$i/100)
+        J[l+1,] <- D[l,]*(input$i/100)
+        
+        P[l,] <- J[l,]+A
+        P[input$k,] <- J[input$k,]+A
+      }
+      A <- round(A,2)
+      J <- round(J,2)
+      D <- round(D,2)
+      P <- round(P,2)
+      
+      Epoca <- seq(1:input$k)
+      
+      result <- data.frame(
+        "Amortização"=c("-", rep(A,input$k), sum(rep(A,input$k))), 
+        "Juros"=c("-", J, sum(J)), 
+        "Prestação"=c("-", P, sum(P)), 
+        "Dívida"=c(D0, D, "Total"))
+      result <- as.data.table(result)
+      DT::datatable(result, extensions = 'Buttons', options = list(
+        dom = 'Bfrtip',
+        buttons = c('pageLength','copy', 'csv', 'excel', 'pdf', 'print')
+      ))
+      
     }
     
-    Epoca <- seq(1:input$k)
-    
-    result <- data.frame("Época"=c(0, Epoca, "Soma"), 
-                         "Amortização"=c("-", rep(A,input$k), sum(rep(A,input$k))), 
-                         "Juros"=c("-", J, sum(J)), 
-                         "Prestação"=c("-", P, sum(P)), 
-                         "Dívida"=c(D0, D, "-"))
-    result
     
   })
   
   output$msg <-renderText({
     cat("A taxa de juros utilizada é =", input$i,"%")
   })
-    
+  
   {output$resultadoJS <- renderText({
     input$tJS*(input$iJS/100)*input$cJS
   })}
+  
+  {output$resultadoJC <- renderText({
+    input$cJC*((input$iJC/100)+1)^input$tJC
+  })}
+  
+  output$resultadoPR <- DT::renderDataTable({
     
-  }
+    if(input$valuep=="0"||input$ip=="0"||input$kp=="0"){
+      
+      as.data.table(paste("Preencha os valores da coluna lateral"))
+      
+      
+    }else{
+      
+      D <- matrix(nrow = input$kp, ncol = 1)
+      A <- matrix(nrow = input$kp, ncol = 1)
+      J <- matrix(nrow = input$kp, ncol = 1)
+      P <- input$valuep*(((1+(input$ip/100))^input$kp)*(input$ip/100))/(((1+(input$ip/100))^input$kp)-1)
+      D0 <- input$valuep
+      J[1]<- D0*(input$ip/100)
+      A[1]<- P-J[1]
+      D[1]<- D0-A[1]
+      for(l in 2:((input$kp))){
+        J[l]=D[l-1]*(input$ip/100)
+        A[l,] <- P-J[l,]
+        D[l,] <- D[l-1,]-A[l,]
+      }
+      A <- round(A,2)
+      J <- round(J,2)
+      D <- round(D,2)
+      P <- round(P,2)
+      
+      
+      
+      Epoca <- seq(1:input$kp)
+      
+      result <- data.frame("Parcela"=c(0, Epoca, "Soma"), 
+                           "Prestação"=c("-", rep(P,input$kp), sum(rep(P,input$kp))), 
+                           "Juros"=c("-", J, sum(J)), 
+                           "Amortização"=c("-", A, sum(A)), 
+                           "Dívida"=c(D0, D  , "-"))
+      result <- as.data.table(result)
+      DT::datatable(result, extensions = 'Buttons', options = list(
+        dom = 'Bfrtip',
+        buttons = c('pageLength','copy', 'csv', 'excel', 'pdf', 'print')
+      ))
+      
+    }
+    
+    
+  })
+  output$resultadoP <- renderText({
+    paste("A taxa de juros utilizada é ", input$ip,"%")
+  })
+  
+}
 
 shinyApp(ui, server)
---
+++ sobre.md
@@ -0,0 +1,41 @@
+O aplicativo
+------------
+
+
+Esse aplicativo tem como objetivo facilitar as operações de um invesntimento.
+Apresentando quanto você ira pagar no sistema SAC E PRICE, além de garantir que você possa fazer o donwload desses resultados de forma rapida.
+i
+
+A monografria pode ser acessada neste link:
+<http:// >
+
+Os scripts utilizados estão disponíveis neste link:
+<https: >
+
+Autores
+-------
+
+**José Vitor Novaes** é graduando em Licenciatura em Matématica  na UFV. Possui
+interesse em Matematica Computacional e Matematica Financeira.
+
+Contato: jose.novaes@ufv.br
+
+**Luiz Gustavo Perona ** é Professor Adjunto na Universidade Federal de Viçosa - Campus Florestal 
+Grande do Norte. É Doutor em Sistemas Dinamicos  e tem interesse nas áreas de
+.
+
+
+
+### Algumas Observações:
+
+-   
+
+-   .
+
+-   
+
+-   .
+
+-   .
+
+
